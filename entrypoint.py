b3aeabf73585d9ff05ee8a0a2efb0b3af010a5ce
danx0r <dan.miller@hiqlabs.com>
2018-06-01 16:30:54 -0700

    hhtps

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import os, time, sys
-os.system("apachectl start")
+os.system("a2enmod ssl; a2ensite default-ssl; apachectl start")
 
 cmd = "tail -fc 50000 /var/log/apache2/access.log /var/log/apache2/error.log"
 os.system(cmd)
